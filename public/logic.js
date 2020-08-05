44eef5cfa84d19fb04f5bce556484da346586e09
cameronbrock <cameronbrock@tamu.edu>
2020-08-05 17:40:02 -0500

    Progress

--- public/logic.js
+++ public/logic.js
@@ -1,71 +1,35 @@
 
-/*
-$('document').ready(() => {
-	$('h1').hide();
-});
-*/
-/*
-$('document').ready(() => {
-	$('.input-imperial').hide();
-});
-*/
-/*
-$('document').ready(() => {
-	$('#input-metric').hide();
-	$('#submit-button').click(() => {
-		//alert('Button has been pressed.');
-		$('#input-imperial').hide();
-		$('#input-metric').show();
-		$('#units').html('Metric');
-	});
-});
-*/
-/*
-// Check whether switch is toggled to imperial or metric.
-$('document').ready(() => {
-	$('#input-metric').hide();
-	if ($('#sw1').is(':checked')) {
-		alert('SW1 is checked.');
-		$('#input-imperial').hide();
-		$('#input-metric').show();
-	}
-	
-});
-*/
-/*
-$('document').ready(() => {
-	if ($('#sw1').prop('checked')) {
-		alert('CHECKED.');
-	}
-	else {
-		alert('UNCHECKED.');
-	}
-});
-*/
 
 $('document').ready(() => {
 	
 	$('#input-metric').hide();
-	
+	$('#results').hide();
+	$('#units').html('Imperial');
+
+	// Alter the document according to whether the user specifies
+	// metric or imperial units.	
+	var fade_time = 200;
 	$('#sw1').click(() => {
 		if ($('#sw1').prop('checked')) {
-			$('#input-imperial').hide();
-			$('#input-metric').show();
+			$('#input-imperial').fadeOut(fade_time, () => {
+				$('#input-metric').fadeIn(fade_time);
+			});
+			$('#units').html('Metric');
 		}
 		else {
-			$('#input-imperial').show();
-			$('#input-metric').hide();
+			$('#input-metric').fadeOut(fade_time, () => {
+				$('#input-imperial').fadeIn(fade_time);
+			});
+			$('#units').html('Imperial');
 		}
 	});
 	
+	// When form is submitted, show the results in the #results div.
+	$('#submit-button').click(() => {
+		$('#results').fadeOut(fade_time, () => {
+			$('#results').fadeIn(fade_time);
+		});
+	});
+	
 });
-
-
-
-
-
-
-
-
-
 
