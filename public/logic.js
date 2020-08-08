b1a7b303958065068c95015ca02263525db4bbee
cameronbrock <cameronbrock@tamu.edu>
2020-08-08 16:19:40 -0500

    Progress

--- public/logic.js
+++ public/logic.js
@@ -1,6 +1,24 @@
 
+const port_num = '1337';
 
 $('document').ready(() => {
+	
+	var BMI_result = {};
+	
+	//var socket = io();
+	var socket = io.connect();
+	socket.on('bmi-result', (result) => {
+		//alert('BMI = ' + result.bmi + '\nClassification = ' + result.classification);
+		//$('#BMI-val').html(result.bmi);
+		//$('#BMI-class').html(result.classification);
+		//BMI_result = result;
+		$('#results').fadeOut(fade_time, () => {
+			$('#BMI-val').html(result.bmi);
+			$('#BMI-class').html(result.classification);
+			$('#results').fadeIn(fade_time);
+		});
+		
+	});
 	
 	$('#input-metric').hide();
 	$('#results').hide();
@@ -30,20 +48,24 @@ $('document').ready(() => {
 		// Send post request to server.
 		var resp_received = false;
 		if ($('#sw1').prop('checked')) {
-			var post_data = {
+			var hw_data = {
 				unit_sys: 'Metric',
 				height_cm: parseFloat( $('#height_metric').val() ),
 				weight_kg: parseFloat( $('#weight_metric').val() )
 			};
 		}
 		else {
-			var post_data = {
+			var hw_data = {
 				unit_sys: 'Imperial',
 				height_ft: parseFloat( $('#height_ft_imp').val() ),
 				height_in: parseFloat( $('#height_in_imp').val() ),
 				weight_lbs: parseFloat( $('#weight_imp').val() )
 			};
 		}
+		
+		socket.emit('bmi-submit', hw_data);
+		
+		/*
 		$('#BMI-info').html(post_data.unit_sys);
 		
 		var post_request = {
@@ -55,11 +77,15 @@ $('document').ready(() => {
 		};
 		
 		fetch('/', post_request);
-		
+		*/
 		// Fade out original results and fade in new results.
+		/*
 		$('#results').fadeOut(fade_time, () => {
+			$('#BMI-val').html(BMI_result.bmi);
+			$('#BMI-class').html(BMI_result.classification);
 			$('#results').fadeIn(fade_time);
 		});
+		*/
 	});
 	
 	// When exit icon is clicked, remove results.
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const parser = require('body-parser');
+const socketIO = require('socket.io');
 
 const app = express();
 const port_num = 1337;
@@ -10,19 +11,46 @@ var json_parser = parser.json();
 app.use(express.static(__dirname + '/public'));
 app.use(parser.json());
 
+var  server = app.listen(port_num, () => {
+	console.log('Starting server (port = ' + port_num + ')...');
+});
+
+var io = socketIO(server);
+
 // Compute the BMI given height, weight, and unit system (Imperial or Metric).
 // Height is measured either in inches (Imperial) or centimeters (Metric).
 // Weight is measured either in pounds (Imperial) or kilograms (Metric).
 function compute_BMI(height, weight, unit_sys) {
 	if (unit_sys == 'Imperial') {
-		return (703 * weight) / (height * height);
+		var bmi = (703 * weight) / (height * height);
 	}
 	else if (unit_sys == 'Metric') {
-		return (10000 * weight) / (height * height);
+		var bmi = (10000 * weight) / (height * height);
 	}
 	else {
 		throw new Error(unit_sys + ' is not a valid unit system. Use either \'Imperial\' or \'Metric.\'');
 	}
+	
+	// Round BMI to two decimal places.
+	bmi = Math.round(100 * bmi);
+	bmi /= 100;
+	return bmi;
+}
+
+function classify_BMI(bmi) {
+	if (bmi < 18.5) {
+		return 'Underweight';
+	}
+	if (bmi < 25) {
+		return 'Normal';
+	}
+	if (bmi < 30) {
+		return 'Overweight';
+	}
+	if (bmi < 40) {
+		return 'Obesity';
+	}
+	return 'Extreme obesity';
 }
 
 app.get('/', (req, res) => {
@@ -60,6 +88,30 @@ app.post('/', json_parser, (req, res) => {
 	}
 });
 
-app.listen(port_num, () => {
-	console.log('Starting server (port = ' + port_num + ')...');
+io.sockets.on('connection', (socket) => {
+
+	socket.on('bmi-submit', (hw_data) => {
+		console.log('Received height/weight data.');
+		console.log('Weight = ' + hw_data.weight_lbs);
+		
+		var unit_sys = hw_data.unit_sys;
+		if (unit_sys == 'Imperial') {
+			var height = 12 * hw_data.height_ft + hw_data.height_in;
+			var weight = hw_data.weight_lbs;			
+		}
+		else if (unit_sys == 'Metric') {
+			var height = hw_data.height_cm;
+			var weight = hw_data.weight_kg;
+		}
+		var BMI = compute_BMI(height, weight, unit_sys);
+		var BMI_class = classify_BMI(BMI);;
+		
+		console.log('Computed BMI = ' + BMI);
+		socket.emit('bmi-result', {
+			bmi: BMI,
+			classification: BMI_class
+		});
+	});
+
 });
+
